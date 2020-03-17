73b40f6592da71e9fce5982db6d60ca6b4cd8d4e
阿男 <l.weinan@gmail.com>
2020-03-17 19:21:48 +0800

    update

--- src/main/java/io/weli/resteasy/xss/MyApplication.java
+++ src/main/java/io/weli/resteasy/xss/MyApplication.java
@@ -4,5 +4,5 @@ import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
 @ApplicationPath("/rest")
-public class RESTApp extends Application {
+public class MyApplication extends Application {
 }
